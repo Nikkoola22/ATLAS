2e28ea39b3e415347ac981574bcfc093dcc5e65d
Nikkoola22 <165650747+Nikkoola22@users.noreply.github.com>
2025-09-23 22:53:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// On détecte si on build pour GitHub Pages ou Vercel
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,5 +15,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/ATLAS/',
+  base: isGithubPages ? '/ATLAS/' : '/', // GitHub Pages : /ATLAS/ | Vercel : /
 });
